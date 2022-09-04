7fef33cb5e5bb5379521455151f68b2da919e63e
Jay Bhogayata <jaybhogayata53@gmail.com>
2022-09-04 12:55:17 +0530

    Hero section mobile responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="bg-[#1b1b1b] flex w-full lg:px-9 px-4 lg:flex-row flex-col space-y-4">
       <div className=" lg:w-3/4 flex  lg:flex-row flex-col  bg-purple  mt-24 mx-auto ">
-        <div className="w-2/5 mx-auto">
+        <div className="lg:w-2/5 mx-auto">
           <h1 className="space-y-5    text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 font-mono   text-5xl  font-bold ">
             Hi , I'm Jay Fullstack Web Developer.
           </h1>
